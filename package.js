9241dedea2f8de11dbbfc07807279e36fd65b106
권정빈 <wjdqls1342@gmail.com>
2017-08-04 01:43:05 +0900

    권정빈 / react-router version update

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'datamaker:react-router-ssr',
-  version: '3.1.7',
+  version: '3.1.8',
   summary: 'Server-side rendering for react-router and react-meteor-data rehydratating Meteor subscriptions',
   git: 'https://github.com/datamaker/meteor-react-router-ssr.git',
   documentation: 'README.md'
@@ -35,7 +35,7 @@ Package.onUse(function(api) {
     'autopublish@1.0.0',
     'tmeasday:publish-counts@0.7.0',
     'promise@0.5.1'
-  ], 'server', {weak: true})
+  ], 'server', {weak: true});
 
   api.export('ReactRouterSSR');
   api.mainModule('lib/react-router-ssr.js');

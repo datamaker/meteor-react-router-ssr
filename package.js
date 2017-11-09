49cfa27dd13bd11035972814c836eb8db925c32a
JungBin Kwon <wjdqls1342@naver.com>
2017-11-09 23:55:32 +0900

    Update package.js

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'datamaker:react-router-ssr',
-  version: '3.2.0',
+  version: '3.2.1',
   summary: 'Server-side rendering for react-router and react-meteor-data rehydratating Meteor subscriptions',
   git: 'https://github.com/datamaker/meteor-react-router-ssr.git',
   documentation: 'README.md'
@@ -18,7 +18,7 @@ Package.onUse(function(api) {
     'ecmascript',
     'tracker',
     'minimongo@1.0.0',
-    'datamaker:fast-render@2.18.0',
+    'datamaker:fast-render@2.19.0',
     'datamaker:inject-data@2.0.0',
     'tmeasday:check-npm-versions@0.2.0'
   ]);
